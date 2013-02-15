8bc182a58c0136877f907b3747a04c5b28ccad6e
philip <philip.deegan@gmail.com>
2013-02-15 15:51:30 +0000

    CliArgs/string/except

---
+++ inc/kul/except.hpp
@@ -0,0 +1,35 @@
+/*
+ * exception.hpp
+ *
+ *  Created on: Feb 15, 2013
+ *      Author: philip
+ */
+
+#ifndef _EXCEPTION_HPP_
+#define _EXCEPTION_HPP_
+
+#include <stdexcept>
+
+#include "kul/string.hpp"
+
+namespace kul{
+
+class Exception : public std::runtime_error{
+	private:
+		const char* f;
+		const int l;
+		Exception& operator=(const Exception& e){ return *this;}
+	public:
+		~Exception() noexcept{}
+		Exception(const char*file, const int line, std::string s) : std::runtime_error(s), f(file), l(line){}
+		Exception(const Exception& e) : std::runtime_error(e.what()), f(e.f),  l(e.l){}
+		const char* what() const noexcept{ return std::runtime_error::what();}
+		const std::string debug() const {return std::string(std::string(f)
+			+ " : " + kul::st_d::string::toString(l) + " : " + std::string(std::runtime_error::what()));}
+		const char* file() const { return f;}
+		const int& line() const { return l;}
+};
+
+
+};
+#endif /* _EXCEPTION_HPP_ */
---
+++ inc/kul/string.hpp
@@ -0,0 +1,60 @@
+/*
+ * string.hpp
+ *
+ *  Created on: Feb 14, 2013
+ *      Author: philip
+ */
+
+#ifndef _STRING_HPP_
+#define _STRING_HPP_
+
+#include "kul/ext/google.hpp"
+
+#include <string>
+#include <string.h>
+#include <vector>
+
+namespace kul { namespace st_d{
+
+class string{
+	public:
+		static std::string toString(const int& i){
+			std::stringstream out;
+			out << i;
+			return out.str();
+		}
+		static void replaceFirst(std::string& s, const char* f, const char* r){
+
+			s.replace(s.find(f), strlen(f), r);
+		}
+		static void replaceAll(std::string& s, const char* f, const char* r){
+
+			while(s.find(f) != std::string::npos){
+				replaceFirst(s, f, r);
+			}
+		}
+		static std::vector<std::string> split(const std::string& s, const char& delim='\n'){
+			std::vector<std::string> ss;
+			std::string l;
+			std::stringstream stream(s);
+			while(std::getline(stream, l, delim)) {
+				if(l.compare("") != 0)
+					ss.push_back(l);
+			}
+			return ss;
+		}
+		static std::vector<std::string> split(const std::string& s, const std::string& delim="\n"){
+			std::string l = s;
+			std::vector<std::string> ss;
+			int pos = 0;
+			while((pos = s.find(delim, pos + 1)) != std::string::npos){
+				ss.push_back(l.substr(0, pos));
+				l = l.substr(pos);
+			}
+			ss.push_back(l);
+			return ss;
+		}
+};
+
+};};
+#endif /* _STRING_HPP_ */
