2f2578b4a7e6f85261dae50c18feb92ee5aac665
Philip Deegan <philip.deegan@gmail.com>
2013-05-16 18:08:20 +0100

    Update to test - add join to threads

--- tst/kul.test.hpp
+++ tst/kul.test.hpp
@@ -67,6 +67,7 @@ class test{ public: test(){
 	kul::threading::Ref<TestThreadObject> ref(tto1);
     kul::threading::Thread th(ref);
     th.run();
+    th.join();
     th.detach();
     th.interrupt();
     tto1.print();
@@ -75,6 +76,7 @@ class test{ public: test(){
     kul::threading::CRef<TestThreadObject> cref(tto2);
     kul::threading::Thread th2(cref);
     th2.run();
+    th2.join();
     th2.detach();
     th2.interrupt();
     tto2.print();
@@ -82,6 +84,7 @@ class test{ public: test(){
 	TestThreadObject tto3;
     kul::threading::Thread th1(tto3);
     th1.run();
+    th1.join();
     th1.detach();
     th1.interrupt();
     tto3.print();
