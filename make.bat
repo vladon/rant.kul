5f82edb415abe46612b9a404b3c4803e1b0bd3e2
Philip Deegan <philip.deegan@gmail.com>
2015-01-16 13:28:47 +0000

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,11 +1,16 @@
 
 
+
 if "%1"=="" GOTO blank
 if "%1"=="init" GOTO init
 else GOTO eof
 
 :init
 SET PWD=%CD%
+CALL :get
+GOTO eof
+
+:get
 IF NOT EXIST %PWD%\ext\sparsehash\trunk (
 	svn co http://sparsehash.googlecode.com/svn/trunk %PWD%\ext\sparsehash\trunk
 )
@@ -18,7 +23,7 @@ GOTO eof
 SET OWD=%PWD%
 SET PWD=%CD%\ext\kul\master
 
-CALL :init
+CALL :get
 
 IF NOT EXIST %PWD%\ext\pugixml\trunk\bin mkdir %PWD%\ext\pugixml\trunk\bin
 %VSCC% %CXXFLAGS%  %INCLUDES% /c /Fo"%PWD%\ext\pugixml\trunk\bin\pugixml.o" "%PWD%\ext\pugixml\trunk\src\pugixml.cpp"
