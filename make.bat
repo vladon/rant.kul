e08cd59ce776d2aba22cea68ba73797166407248
Philip Deegan <philip.deegan@gmail.com>
2015-01-16 13:31:23 +0000

    Update make.bat

--- make.bat
+++ make.bat
@@ -29,7 +29,8 @@ IF NOT EXIST %PWD%\ext\pugixml\trunk\bin mkdir %PWD%\ext\pugixml\trunk\bin
 %VSCC% %CXXFLAGS%  %INCLUDES% /c /Fo"%PWD%\ext\pugixml\trunk\bin\pugixml.o" "%PWD%\ext\pugixml\trunk\src\pugixml.cpp"
 %VSCAR% /OUT:"%PWD%\ext\pugixml\trunk\bin\pugixml.lib" /NOLOGO /LTCG %PWD%\ext\pugixml\trunk\bin\pugixml.o
 
-IF NOT EXIST %PWD%\bin mkdir %PWD%\bin
+RD /S/Q %PWD%\bin
+MKDIR %PWD%\bin
 FOR /f %%f in ('dir /b %PWD%\src') do %VSCC% %CXXFLAGS% %INCLUDES% /c /Fo"%PWD%\bin\%%f.o" "%PWD%\src\%%f" 
 
 FOR /f %%f in ('dir /b %PWD%\bin') do SET OBJECTS=!OBJECTS! %PWD%\bin\%%f
