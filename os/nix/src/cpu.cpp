66707d03795811c53e9f1a4aabb39ecba92cc31e
Philip Deegan <philip.deegan@gmail.com>
2015-03-31 13:16:41 +0100

    Copyright comment

--- os/nix/src/cpu.cpp
+++ os/nix/src/cpu.cpp
@@ -6,7 +6,7 @@ Created on: 1 Jan 2013
 
 Copyright (c) 2013, Philip Deegan
 
-This file is part of kul.nix (The Kommon Usage Library for Linux Distros).
+This file is part of kul (The Kommon Usage Library).
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
